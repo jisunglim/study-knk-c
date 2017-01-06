ce9b315100a4af5b0744c20fcb104f571d3b0428
jaylim <iejisung@gmail.com>
2017-01-06 12:44:54 +0900

    ch20 bitwise basic

--- ch20/access-bits/access-bits.c
+++ ch20/access-bits/access-bits.c
@@ -40,3 +40,29 @@ bool test_bits(Bits b, int j)
 {
   return ((*b) & 1 << j);
 }
+
+void set_bit_field(Bits b, unsigned int i)
+{
+  unsigned int cleaner = 0x0000;
+  int j, start = 0, end = 15;
+
+  while (!test_bits(&i, start++))
+    ;
+  while (!test_bits(&i, end--))
+    ;
+
+  while (start <= end) {
+    set_bits(&cleaner, start++);
+    set_bits(&cleaner, end--);
+  }
+
+  (*b) = ((*b) & ~cleaner) | i;
+}
+
+Bits get_bit_field(Bits b, unsigned int i)
+{
+  Bits new = malloc(sizeof(bits_type));
+  set_bit_field(new, (*b) & i);
+
+  return new;
+}
--- ch20/access-bits/client.c
+++ ch20/access-bits/client.c
@@ -4,6 +4,7 @@
 int main(void)
 {
   Bits b = create_bits();
+  Bits n;
 
   set_all_bits(b);
   printf("set_all_bits(b) : %d\n", *b);
@@ -19,6 +20,18 @@ int main(void)
 
   clear_bits(b, 3);
   printf("clear_bits(b, 3) : %d\n", *b);
+
+  set_bit_field(b, 0x0032);
+  printf("set_bit_field(b, 0x0032) : %d\n", *b);
+
+  n = get_bit_field(b, 0x00F0);
+  printf("get_bit_field(b, 0x00F0) : %d\n", *n);
+
+  n = get_bit_field(b, 0x000F);
+  printf("get_bit_field(b, 0x00F0) : %d\n", *n);
+
+  set_bit_field(b, 0x00F0);
+  printf("set_bit_field(b, 0x00F0) : %d\n", *b);
 
   destroy_bits(b);
 }
